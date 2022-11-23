ab4f49ff127f858f4801860f74c966fc09edfb12
welllucky <wellingtonbat.wl@gmail.com>
2022-11-23 14:39:51 -0300

    Criação do projeto

---
+++ src/Entidades/Atendente.java
@@ -0,0 +1,5 @@
+package Entidades;
+
+public class Atendente extends Usuario{
+    
+}
---
+++ src/Entidades/Usuario.java
@@ -0,0 +1,50 @@
+package Entidades;
+
+import Attr.*;
+import Objetos.*;
+
+public abstract class Usuario {
+    private static int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private float classificação;
+    private String equipe;
+    
+    public static int getId() {
+        return id;
+    }
+    public static void setId(int id) {
+        Usuario.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public float getClassificação() {
+        return classificação;
+    }
+    public void setClassificação(float classificação) {
+        this.classificação = classificação;
+    }
+    public String getEquipe() {
+        return equipe;
+    }
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+}
